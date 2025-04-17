0faa153e9f3f5e598c185621f53a7d1a9f63c1d9
Pedro Germano <pedrogermano232@gmail.com>
2025-04-17 13:52:10 -0300

    project 2

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,13 +7,16 @@ const Todo = () => {
 
   const handleSubmit = () => {
     setTodos((todos) => {
-      todos.concat({
+      return todos.concat({
         text: input,
         id: Math.floor(Math.random() * 10),
       });
     });
     setInput("");
   };
+
+  const removeTodo = (id) =>
+    setTodos((todos) => todos.filter((t) => t.id !== id));
 
   return (
     <div className="container">
@@ -25,6 +28,17 @@ const Todo = () => {
       />
 
       <button onClick={handleSubmit}>Submit</button>
+
+      <ul className="todos-list">
+        {todos.map(({ text, id }) => (
+          <li className="todo" key={id}>
+            <span>{text}</span>
+            <button className="close" onClick={() => removeTodo(id)}>
+              x
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
