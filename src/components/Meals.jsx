b7207605f28b2fe9c6852177b2ee554f9ce31b8a
Pedro Germano <pedrogermano232@gmail.com>
2025-04-19 13:43:11 -0300

    meals project done

---
+++ src/components/Meals.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useState } from "react";
+
+import "../styles.css";
+
+const Meals = () => {
+  const [items, setItems] = useState([]);
+
+  useState(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        // console.log(res.data.meals);
+        setItems(res.data.meals);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const itemsList = items.map(({ strMeal, strMealThumb, idMeal }) => {
+    return (
+      <section className="card">
+        <img src={strMealThumb} alt={strMeal} />
+        <section className="content">
+          <p>{strMeal}</p>
+          <p>#{idMeal}</p>
+        </section>
+      </section>
+    );
+  });
+
+  return <div className="items-container">{itemsList}</div>;
+};
+
+export default Meals;
